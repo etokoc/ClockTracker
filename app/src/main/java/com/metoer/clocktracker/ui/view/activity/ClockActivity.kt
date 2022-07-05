abd904051f1241103e7140c386d4a0ac42bc5390
Ömer Yavuzyiğit <yavuzyigitomer52@gmail.com>
2022-07-06 00:45:29 +0300

    sayfalar arası geçiş ve bottomnavigationview tasrımı

--- app/src/main/java/com/metoer/clocktracker/ui/view/activity/ClockActivity.kt
+++ app/src/main/java/com/metoer/clocktracker/ui/view/activity/ClockActivity.kt
@@ -2,11 +2,17 @@ package com.metoer.clocktracker.ui.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.metoer.clocktracker.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class ClockActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController=findNavController(R.id.home_fragment)
+        clockMenuBar.setupWithNavController(navController)
     }
 }
