18b8d3450cccfde1a35259747835ee0c26b404f9
Ertugrul KOC <etoertugrul.koc@gmail.com>
2022-07-06 00:12:18 +0300

    Fragment'ler eklendi. Navigation eklendi.

---
+++ app/src/main/java/com/metoer/clocktracker/ui/view/fragment/CountDownFragment.kt
@@ -0,0 +1,19 @@
+package com.metoer.clocktracker.ui.view.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.metoer.clocktracker.R
+
+class CountDownFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_count_down, container, false)
+    }
+}
